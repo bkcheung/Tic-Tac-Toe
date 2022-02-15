adb265c5ed18c0928bcae3b72b9409e9756ee6d1
bkcheung <b.cheung@hotmail.com>
2022-02-14 23:43:46 -0800

    Add addPlayers(), changeState when div is clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,37 +1,51 @@
 let players = 0;
 
-const gameBoard = (() => {
-    this.gameArray = ['X', 'O', 'O', 'X', 'O', 'O', 'X', 'X', 'O'];
-    
-    const board = document.querySelector('#gameBoard');
+const addPlayers = () => {
+    let player1 = player(document.getElementById('player1').value);
+    let player2 = player(document.getElementById('player2').value)
+    let greeting = `${player1.playerName} ${player1.symbol} versus ${player2.playerName} ${player2.symbol}`;
 
-    this.createDiv = ((state) => {
+    document.getElementById("playerForm").style.display = "none";
+    document.getElementById("playerGreeting").innerHTML = greeting;
+}
+
+const gameBoard = (() => {
+    this.gameArray = [0,0,0,0,0,0,0,0,0];
+    
+    let board = document.querySelector('#gameBoard');
+
+    this._createDiv = ((index) => {
+        state = gameArray[index];
         let squareDiv = document.createElement('div');
         squareDiv.classList.add('squareDiv');
-        state ? squareDiv.innerHTML = state : squareDiv.innerHTML = '';
+        squareDiv.setAttribute('index', index);
+        squareDiv.setAttribute('onclick', `changeState(${index})`);
+        state ? squareDiv.innerHTML = state : squareDiv.innerHTML = ' ';
         board.append(squareDiv);
     })
 
     this.render = (() => {
+        board.innerHTML = '';
         for(i=0; i<9; i++){
-            createDiv(gameArray[i]);
+            _createDiv(i);
         }
+    })
+    this.changeState = ((index) => {
+        console.log(index);
+        gameArray[index] = 'X';
+        render();
     })
 
     return {
-        render
+        render, changeState
     }
 })();
 
 const player = (playerName) => {
-    const symbol = players ? 'X' : 'O';
+    const symbol = players ? 'O' : 'X';
     players++;
-
     return {playerName, symbol};
 };
 
 gameBoard.render();
-
-let bonnie = player('Bonnie');
-let kevin = player('Kevin');
 
